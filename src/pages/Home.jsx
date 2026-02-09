90d88d6182ea1d8ebb3a3f9e9daee04ff103d7c1
013Ratchanon <dewtete@gmail.com>
2026-02-09 15:10:34 +0700

    no-hero

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,9 @@
-import HeroSection from "../components/HeroSection";
 import CategoryTabs from "../components/CategoryTabs";
 import ContentSection from "../components/ContentSection";
 
 const Home = () => {
   return (
     <>
-      <HeroSection />
       <CategoryTabs />
       <ContentSection />
     </>
